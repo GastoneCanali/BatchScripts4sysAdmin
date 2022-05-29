9bd0c03445b439d56f5be8a4c5a703a1db26583c
Gastonecanali <gas@armadillo.it>
2022-05-29 18:59:48 +0200

    Add files via upload

---
+++ Printer/DelOLD_InstNEW_prn.cmd
@@ -0,0 +1,28 @@
+@echo off
+setlocal
+rem sopprime std output e err output 
+set $nolog= ^> nul 2^>^&1
+
+:: Esempio se la stampante vecchia si chiama \\SRVtitan\hp laser amm
+:: e la nuova \\SRVtitan\hp laserAmm
+SET printServer="SRVtitan"
+set oldPrinter="hp laser amm"
+set newPrinter="LaserAmm"
+
+:: verifico l'esistenza della stampante vecchia
+   reg query "HKEY_CURRENT_USER\Printers\Connections\,,%printServer%,%oldPrinter%" %$nolog% && set "$Prn_exist=Y"
+   If /i [%$Prn_exist%]==[y] (
+       echo "%oldPrinter% presente: %$Prn_exist%"
+	   echo Cancello
+       RUNDLL32.EXE printui.dll,PrintUIEntry /q /dn  /n "\\%printServer%\%oldPrinter%"	  
+   )
+   
+:: verifico l'esistenza della stampante nuova
+   reg query "HKEY_CURRENT_USER\Printers\Connections\,,%printServer%,%newPrinter%" %$nolog% && set "$Prn_exist=Y" 
+   If /i [%$Prn_exist%]==[y] (
+       echo "Nuova stamapante %newPrinter% presente
+   ) else (
+	   echo "Nuova stamapante %newPrinter% NON presente, installo
+       RUNDLL32.EXE printui.dll,PrintUIEntry /q /u  /in  /n "\\%printServer%\%newPrinter%"   
+   )
+
